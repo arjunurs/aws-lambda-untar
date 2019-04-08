693b0f50f2c8e02635fcbaa60fa5d01c79dfd83d
Arjun Urs <arjun.urs@snowflake.com>
2019-04-08 16:33:44 -0700

    Fixed filter suffix

--- main.tf
+++ main.tf
@@ -72,6 +72,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.untar_lambda_lambda_function.arn}"
     events              = ["s3:ObjectCreated:*"]
-    filter_suffix       = ".zip"
+    filter_suffix       = ".tar.gz"
   }
 }
